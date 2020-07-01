88837f566fda6398a86c4c0b0f1db8e7f26c224d
Seina Ishibashi <ishibashi.mcs@gmail.com>
2020-07-01 17:33:33 +0900

    Add: Booksの新規作成機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :books, only: [:index]
 
   namespace :api do
-    resources :books, only: [:index, :show]
+    resources :books, only: [:index, :show, :create]
   end
 end
