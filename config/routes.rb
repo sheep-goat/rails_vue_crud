bbba0b6c2a6b7510567bd2c0838d443e8d553411
Seina Ishibashi <ishibashi.mcs@gmail.com>
2020-07-01 18:00:20 +0900

    Add: Booksの削除機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :books, only: [:index]
 
   namespace :api do
-    resources :books, only: [:index, :show, :create, :update]
+    resources :books, only: [:index, :show, :create, :update, :destroy]
   end
 end
