ccf453b2443c3e51f92cc44378adfae72af8e9bb
Seina Ishibashi <ishibashi.mcs@gmail.com>
2020-07-01 15:42:56 +0900

    Add: BooksのAPIを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :books, only: [:index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :books, only: [:show]
+  end
 end
